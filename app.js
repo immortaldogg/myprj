faa2692db37cb4d6097c978a6d74d5ca100f7751
laminh711 <laminh711@gmail.com>
2018-11-21 00:05:45 +0700

    gulp sass config + html image url fix

---
+++ app.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(req, res) {
+    console.log('Request: ', req.url);
+
+    var filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end(`Sorry, check with the site admin for error: ${error.code}..\n`);
+                res.end();
+            }
+        } 
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    })
+}).listen(8125);
+console.log('Server running at http://localhost:8125/');

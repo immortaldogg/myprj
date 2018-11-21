10f3a67d56e671169812645a5584b421e40de559
laminh711 <laminh711@gmail.com>
2018-11-21 18:48:12 +0700

    navigation bar

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,32 @@
-var gulp = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', () => {
-    gulp.src('sass/**/*.scss')
+function autocompile(cb) {
+    console.log("Some files changed. Updating...")
+    src('./sass/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
-})
+        .pipe(dest('./css/'));
+    
+    cb();
+}
 
-// Watch task
-gulp.task('default', () => {
-    gulp.watch('sass/**/*.scss', ['styles']);
-})
+// gulp.task('styles', () => {
+//     gulp.src('sass/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('./css/'));
+// })
+
+function myprj_watcher(cb) {
+    // Watch task
+    watch('./sass/**/*.scss', autocompile);
+    cb();
+}
+
+function myprj_default(cb) {
+    console.log("Watching changes...")
+    cb();
+}
+
+exports.watch = myprj_watcher;
+
+exports.default = series(myprj_default, myprj_watcher);

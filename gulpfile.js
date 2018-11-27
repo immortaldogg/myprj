70a8a0efd31f885e7c221d91ae91114bc6cc5db2
laminh711 <laminh711@gmail.com>
2018-11-27 18:40:30 +0700

    trying out semantic

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,12 @@
-const { src, dest, series, watch } = require('gulp');
+var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-function compile_scss(cb) {
-    console.log("Some files changed. Updating...")
-    src('./sass/main.scss')
+gulp.task('styles', () => {
+    gulp.src('scss/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('./css/'));
-    
-    cb();
-}
+        .pipe(gulp.dest('./css/custom/'));
+})
 
-// gulp.task('styles', () => {
-//     gulp.src('sass/**/*.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest('./css/'));
-// })
-
-function myprj_watcher(cb) {
-    // Watch task
-    watch(['./sass/**/*.scss', './sass/main.scss'], compile_scss);
-    cb();
-}
-
-function myprj_default(cb) {
-    console.log("Default task running...");
-    cb();
-}
-
-exports.compile = compile_scss;
-exports.watch = myprj_watcher;
-exports.default = series(myprj_default, myprj_watcher);
+gulp.task('watch', () => {
+    gulp.watch(['scss/**/*.scss', 'scss/main.scss'], ['styles']);
+})
